d74f8ef5be04758b165d45d3ed72eae1ad784e53
kerry2021 <aca.kerry.li@gmail.com>
2025-07-16 17:38:09 -0400

    renamed the user end point and added get functionalites

--- api/users.py
+++ api/users.py
@@ -2,6 +2,7 @@ import json
 import psycopg2
 import os
 from http.server import BaseHTTPRequestHandler
+from urllib.parse import urlparse, parse_qs
 
 def CORS_helper(handler):
     handler.send_header("Access-Control-Allow-Origin", "*")
@@ -53,7 +54,24 @@ class handler(BaseHTTPRequestHandler):
         send_json(self, {"status": "ok"}) 
 
     def do_GET(self):
-        self.send_response(405)
-        self.send_header("Content-type", "text/plain")
-        self.end_headers()
-        self.wfile.write("Method Not Allowed".encode("utf-8"))
+        parsed_path = urlparse(self.path)
+        query_params = parse_qs(parsed_path.query)
+        access_type = query_params.get("access_type", [None])[0]
+        #get all users and filter by access_type
+        conn = psycopg2.connect(os.environ["DATABASE_URL"])
+        cursor = conn.cursor()
+        if access_type == "all":
+            cursor.execute("SELECT user_id, name, pic_link FROM users")
+        elif access_type:
+            cursor.execute("SELECT user_id, name, pic_link FROM users WHERE access_type = %s", (access_type,))
+        else:
+            print("Error: access_type not provided")
+            self.send_response(400)
+
+
+        users = cursor.fetchall()
+        cursor.close()
+        conn.close()
+        user_list = [{"user_id": user[0], "name": user[1], "pic_link": user[2]} for user in users]
+        send_json(self, user_list)
+        
